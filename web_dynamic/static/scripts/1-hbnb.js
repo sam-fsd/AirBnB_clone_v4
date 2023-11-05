b68d96b19ee1c64d06b6561532f772c62a7e379a
Otonda1 <charlesotonda@gmail.com>
2023-11-05 03:51:34 -0800

    current changes in our emmenities jquery

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,6 +1,17 @@
 $(function () {
-	let selectedAmmenities = {};
+	let selectedAmenities = {};
 	$("input").change(function () {
+		const amenityId = $(this).data("data-id");
+		const amenityName = $(this).data("data-name");
 
+		if ($(this).is(":checked")) {
+			selectedAmenities[amenityId] = amenityName;
+		} else {
+			delete selectedAmenities[amenityId];
+
+		}
+
+		const checkedAmenities = Object.values(selectedAmenities).join(', ');
+		$(".amenities h4").text(checkedAmenities);
 	});
 });
